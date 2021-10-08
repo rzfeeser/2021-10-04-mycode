fb520cc9c5f0ec2eabd2e44964bb2545233846c6
z <rzfeeser@users.noreply.github.com>
2021-10-08 17:29:30 +0000

    end of class

---
+++ packaging_tutorial/README.md
@@ -0,0 +1,11 @@
+# Example Package
+
+This is a simple example package. You can use
+[GitHub-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
+
+this is **bold**
+
+*this is also styled*
+
+
---
+++ smallscript.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Learning how to Lint code"""
+
+def main():
+    """run time code"""
+
+
+
+    my_note = "print this string"
+
+
+
+    print(my_note)
+
+
+
+if __name__ == "__main__":
+    main()
